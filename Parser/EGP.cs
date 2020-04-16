9780bd3ccaeedf3252ee809f6903b4490046e43d
Sergey Patoka <sergey.pat98@gmail.com>
2020-04-16 15:49:31 +0500

    Add project files.

---
+++ Parser/EGP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPaS5.Parser
+{
+    public class EGP
+    {
+        public class TvStreamInfo
+        {
+
+        }
+    }
+}
